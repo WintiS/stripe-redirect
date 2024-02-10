a8f22d382519ecafad76ccf11181eb09728c72be
Vitezslav Sima <kviteksima@seznam.cz>
2024-02-10 22:02:23 +0100

    Feat: backend

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -14,7 +14,7 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image" href="https://images.unsplash.com/photo-1545156521-77bd85671d30?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,44 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import {$, component$, useSignal, } from "@builder.io/qwik";
+import type {DocumentHead} from "@builder.io/qwik-city";
+import {server$} from "@builder.io/qwik-city";
+
+export const getLastPartOfUrl = $((url:string) => {
+  const segments = url.split('/');
+  return segments[segments.length - 1];
+})
 
 export default component$(() => {
+  const paymentLink = useSignal("")
+  const redirect = useSignal("#")
+
+
+  const getRedirectURL = server$(async (final) => {
+    const lastPart = await getLastPartOfUrl(final)
+    console.log(lastPart)
+    const res = await fetch(`https://merchant-ui-api.stripe.com/payment-links/${lastPart}`, {
+      method: "POST",
+    })
+    const jsonn = await res.json()
+    return jsonn.completion_behavior.redirect_url
+  })
+
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <div>
+        <form preventdefault:submit onSubmit$={ async () => {
+          redirect.value = "Fetching..."
+          redirect.value = await getRedirectURL(paymentLink.value)
+        }}>
+          <input type="text" placeholder={"Paste the stripe payment link here"} onInput$={(e) => {
+            paymentLink.value = (e.target as HTMLInputElement).value
+          }}/>
+          <input type="submit"/>
+        </form>
+      </div>
+      <div>
+        <h2>The redirect url will appear underneath. Feel free to click it or copy it.</h2>
+        <a href={redirect.value}>{redirect.value}</a>
+      </div>
     </>
   );
 });
